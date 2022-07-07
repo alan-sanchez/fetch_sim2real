a6dae408a80d82e8fa7e7df7d265ede1fca6c9e4
Alan G. Sanchez <sanchala@oregonstate.edu>
2022-07-06 21:58:39 -0700

    Included comments to a couple of functions.

--- src/accumulation_map.py
+++ src/accumulation_map.py
@@ -17,23 +17,46 @@ from geometry_msgs.msg import Point32
 
 class Accumulator:
     """
-    A clas
+    A class that subscribes to the UV direction vectors and publishes a matrix
+    that represents a UV accumulation depth map.
     """
     def __init__(self):
-        # Initialize Subscribers
+        """
+        Function that initializes the subscriber, publisher, and other variables.
+        :param self: The self reference.
+        """
+        # Initialize Subscriber
         self.vector_sub = rospy.Subscriber('/vectors', numpy_msg(Floats), self.irradiance_vectors, queue_size=10)
 
-        self.iter = 0
+        # Initialize Publisher
+
+        # Create an empty list for future storage of the rays that hit.
         self.hit_list = []
+
+        # Create and epty list for the irradiance values for the rays that hit.
         self.ir_list = []
+
+        # Set a previous time as none. This will be used for [ADD HERE]
         self.prev_time = None
 
     def irradiance_vectors(self, vectors):
-        #
+        """
+        Function that determines if they hit the depth map. This returns a distance
+        value in which the inverse square law can be applied to solve the for UV accumulation.
+        :param self: The self reference.
+        :param vectors: Array of floats.
+        """
+        # The end effector location, relative to the base link, is the origin of the vector
         origin = np.array([vectors.data[0], vectors.data[1], vectors.data[2]], dtype=np.double)
+
+        # The direction vectors are then set to v
         v = vectors.data[3:]
+
+        # end is a zero array the size of 1 directional vector
         end = np.array([0,0,0], dtype=np.double)
-        start = rospy.get_time()
+
+        # # Begin Timer
+        # start = rospy.get_time()
 
         hit_list = []
         irradiance = []
