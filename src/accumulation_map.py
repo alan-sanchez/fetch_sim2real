1b2344ae249e14c90302f7bf27b2042ee9447964
Alan G. Sanchez <sanchala@oregonstate.edu>
2022-07-07 21:38:08 -0700

    Added more comments and included more conditional statements for the time exposure.

--- src/accumulation_map.py
+++ src/accumulation_map.py
@@ -39,58 +39,70 @@ class Accumulator:
         # Set a previous time as none. This will be used for [ADD HERE]
         self.prev_time = None
 
-    def irradiance_vectors(self, vectors):
+    def irradiance_vectors(self, msg):
         """
         Function that determines if they hit the depth map. This returns a distance
         value in which the inverse square law can be applied to solve the for UV accumulation.
         :param self: The self reference.
         :param vectors: Array of floats.
         """
-        # The end effector location, relative to the base link, is the origin of the vector
-        origin = np.array([vectors.data[0], vectors.data[1], vectors.data[2]], dtype=np.double)
+        # This resets the prev_time variable to None after a trajectory execution
+        # is complete. This is because there is a 2 second wait time before the user
+        # can generate another random region to disinfect.
+        if (rospy.get_time() - self.prev_time) > 2:
+            self.prev_time = None
 
-        # The direction vectors are then set to v
-        v = vectors.data[3:]
+        # For the first  or reset of the prev_time variable.
+        if self.prev_time == None:
+            self.prev_time = rospy.get_time()
 
-        # end is a zero array the size of 1 directional vector
+        # The end effector location and its referencing the base link.
+        ee_loc = np.array([msg.data[0], msg.data[1], msg.data[2]], dtype=np.double)
+
+        # The direction vectors and their irradiance values are set as a long list
+        dir_ir_vectors = msg.data[3:]
+
+        # end is a zero array where the location of the hit will be stored.
         end = np.array([0,0,0], dtype=np.double)
 
-        # # Begin Timer
+        # # Begin Timer for computed the UV dose (UV irradiance x time exposure)
         # start = rospy.get_time()
 
-        hit_list = []
+        hits = []
         irradiance = []
 
-        for j in range(int(len(v)/4)):
+        # Use for loop to parse directional vector data and check if each hits
+        # a location in the disinfection region.
+        for j in range(int(len(dir_vectors)/4)):
             vector = np.array([v[j*4 + 0],
                                v[j*4 + 1],
                                v[j*4 + 2]], dtype=np.double)
 
-            hit = self.octree.castRay(origin,
+            # Use castRay function in octree class to return a hit location
+            hit = self.octree.castRay(ee_loc,
                                       vector,
                                       end,
                                       ignoreUnknownCells = True,
                                       maxRange = 1.0)
 
-
+            # If there is a hit, then continue next set of computations
             if hit:
-                if self.prev_time == None:
-                    self.prev_time = rospy.get_time()
 
-                if end.tolist() in hit_list:
-                    pass
-                else:
-                    hit_list.append(end.tolist())
+                # Append hit location, end, to a hit_list
+                hits.append(end.tolist())
 
-                    Ray_length = np.sqrt(np.sum((end-origin)**2, axis=0))
-                    dist_ratio = (0.3**2)/Ray_length**2
-                    time_exposure = abs(rospy.get_time() - self.prev_time)
-                    ir =  v[j*4 + 3]
-                    irradiance.append(dist_ratio * time_exposure * ir)
+                # Get euclidean distance from end and end_effector tf.
+                Ray_length = np.sqrt(np.sum((end-ee_loc)**2, axis=0))
+
+                #
+                dist_ratio = (0.3**2)/Ray_length**2
+                time_exposure = abs(rospy.get_time() - self.prev_time)
+                ir =  dir_ir_vectors[j*4 + 3]
+                irradiance.append(dist_ratio * time_exposure * ir)
 
 
         for k in range(len(irradiance)):
-            if hit_list[k] in self.hit_list:
+            if hits[k] in self.hit_list:
                 index = self.hit_list.index(hit_list[k])
                 self.ir_list[index] = irradiance[k] + self.ir_list[index]
 
@@ -110,3 +122,16 @@ if __name__=="__main__":
     rospy.init_node('castRays',anonymous=True)
     CastRays()
     rospy.spin()
+
+    #
+    #
+    # if end.tolist() in hit_list:
+    #     pass
+    # else:
+    #     hit_list.append(end.tolist())
+    #
+    #     Ray_length = np.sqrt(np.sum((end-ee_loc)**2, axis=0))
+    #     dist_ratio = (0.3**2)/Ray_length**2
+    #     time_exposure = abs(rospy.get_time() - self.prev_time)
+    #     ir =  v[j*4 + 3]
+    #     irradiance.append(dist_ratio * time_exposure * ir)
