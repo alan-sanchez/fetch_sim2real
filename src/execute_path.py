9e1d333f2b9de8b50e4a5072bf8c58ebedd0e6b5
Alan G. Sanchez <sanchala@oregonstate.edu>
2022-07-09 14:27:13 -0700

    Included inti_pose() function to the ExecutePath class. Also included more comments.

--- src/execute_path.py
+++ src/execute_path.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+# Import modules
 import sys
 import actionlib
 import subprocess
@@ -9,6 +10,7 @@ import moveit_msgs.msg
 import numpy as np
 import time
 
+# Import message types and other python libraries.
 from threading import Thread
 from moveit_msgs.msg import MoveItErrorCodes, PlanningScene, RobotTrajectory
 from moveit_python import MoveGroupInterface, PlanningSceneInterface
@@ -18,16 +20,22 @@ from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion, PoseArray
 
 class ExecutePath(object):
     """
-    MoveGroupPythonIntefaceTutorial
+    Class that commands the Fetch robot to execute a joint trajectory based on
+    subscribes pose goals.
     """
 
     def __init__(self):
+        """
+        A function that initializes subscriber, publisher, moveit_commander,and
+        planning scene.
+        :param self: The self reference.
+        """
         super(ExecutePath, self).__init__()
         # Initialize subscribers
         self.waypoints_sub = rospy.Subscriber('waypoints', PoseArray, self.go_to_pose)
 
         # Publisher
-        self.duration_pub  = rospy.Publisher('duration', Float32, queue_size=10)
+        self.command_pub  = rospy.Publisher('command', String, queue_size=10)
 
         # First initialize `moveit_commander`
         moveit_commander.roscpp_initialize(sys.argv)
@@ -57,68 +65,61 @@ class ExecutePath(object):
         # self.planning_scene.addBox("Table", size_x=1, size_y=1.5, size_z=0.05, x=1, y=0, z=0.625 )
 
 
-        # Set path_to_goal to the FollowTrajectoryClient Class
-        self.path_to_goal=FollowTrajectoryClient()
-        self.path_to_goal.init_pose()
-        print("====== Press 'Enter' to generate random region =======")
-
-
-
-    def go_to_pose(self,msg):
-        # flag = False
-        self.duration_pub.publish(.5)
-
-        for pose_goal in msg.poses:
-            self.group.set_pose_target(pose_goal)
-            self.group.set_planning_time(5)
-            # plan = group.plan()
-            # self.group.set_max_acceleration_scaling_factor(.5)
-            # if not flag:
-            #     pass
-            # else:
-            #     print("")
-            #     print("time for stop: " + str(time.time()-stop))
-
-
-            # t = time.time()
-            # Now, we call the planner to compute the plan and execute it.
-            plan = self.group.go(wait=True)
-            # Calling `stop()` ensures that there is no residual movement
-            # elapsed = time.time() - t
-            # stop = time.time()
-            # flag = True
-            #
-            # print("time to execute: " + str(elapsed))
-            # print("")
-
-        self.path_to_goal.init_pose()
-        rospy.sleep(2)
+        # Print out instructions for user input to get things started
         print("")
         print("====== Press 'Enter' to generate random region =======")
 
 
+    def go_to_pose(self,msg):
+        """
+        Function that sends pose goals for the Fetch to execute. This function also
+        publishes a string that will command other nodes to run UV accumulation
+        functions.
+        :param self: The self reference.
+        :param msg: The PoseArray message type.
+        """
+        # Publish string command to initiate functions in other nodes
+        self.command_pub.publish("start")
 
+        # Go to each pose goal, using a forloop
+        for pose_goal in msg.poses:
+            self.group.set_pose_target(pose_goal)
+            self.group.set_planning_time(5)
+            plan = self.group.go(wait=True)
 
-class FollowTrajectoryClient(object):
+        # Publish string command to stop UV accumulation mapping from other nodes
+        self.command_pub.publish("stop")
 
-    def __init__(self):
-        rospy.loginfo("Waiting for MoveIt...")
-        self.client = MoveGroupInterface("arm_with_torso", "base_link")
-        rospy.loginfo("...connected")
-        self.scene = PlanningSceneInterface("base_link")
-        self.scene.addBox("keepout", 0.2, 0.5, 0.05, 0.15, 0.0, 0.375)
+        # Go back to initail position
+        self.init_pose()
+
+        # Pause the simulation for a couple of seconds
+        rospy.sleep(2.5)
+
+        # Print out instructions for user input for next disinfection simulation
+        print("")
+        print("====== Press 'Enter' to generate random region =======")
 
     def init_pose(self, vel = .4):
-
+        """
+        Function that sends a joint goal that moves the Fetch's arm and torso to
+        the initial position.
+        :param self: The self reference.
+        :param vel: Float value for arm velocity.
+        """
         # Padding does not work (especially for self collisions)
         # So we are adding a box above the base of the robot
         scene = PlanningSceneInterface("base_link")
-
         scene.addBox("keepout", 0.2, 0.5, 0.05, 0.15, 0.0, 0.375)
 
+        # List of joints that will be in motion
         joints = ["torso_lift_joint", "shoulder_pan_joint", "shoulder_lift_joint", "upperarm_roll_joint",
                   "elbow_flex_joint", "forearm_roll_joint", "wrist_flex_joint", "wrist_roll_joint"]
+
+        # List of joint positions for the initial pose
         pose = [.05, 1.41, 0.30, -0.22, -2.25, -1.56, 1.80, -0.37,]
+
+        # Move to joint position
         while not rospy.is_shutdown():
             result = self.client.moveToJointPosition(joints,
                                                      pose,
@@ -130,10 +131,8 @@ class FollowTrajectoryClient(object):
                 return
 
 
-
 if __name__ == '__main__':
     ## First initialize `moveit_commander`_ and a `rospy`_ node:
-
     rospy.init_node('execute_path', anonymous=True)
     ExecutePath()
     rospy.spin()
